2d5131f446e45793a3d1be366b07b55c66627439
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-06 13:00:11 -0600

    Create basic spring boot project

---
+++ src/main/java/com/example/mumschedpoc/MumSchedPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.mumschedpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumSchedPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumSchedPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mumschedpoc/MumSchedPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mumschedpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MumSchedPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
