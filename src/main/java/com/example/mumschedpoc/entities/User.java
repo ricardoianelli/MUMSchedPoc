794471cd4b560ac92c2fb09d41a4d05b7e0606cc
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-06 13:44:12 -0600

    Add User entity

---
+++ src/main/java/com/example/mumschedpoc/entities/User.java
@@ -0,0 +1,84 @@
+package com.example.mumschedpoc.entities;
+
+import com.example.mumschedpoc.entities.enums.UserRole;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String userName;
+    private String password;
+    private UserRole userRole;
+
+    public User() {
+    }
+
+    public User(Long id, String name, UserRole userRole, String userName, String password) {
+        this.id = id;
+        this.name = name;
+        this.userRole = userRole;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
