8d3590d4493dbb7b5df0f9610dcf618f1e7d0b07
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-06 15:18:06 -0600

    Implement new user endpoints

--- src/main/java/com/example/mumschedpoc/resources/UserResource.java
+++ src/main/java/com/example/mumschedpoc/resources/UserResource.java
@@ -1,20 +1,58 @@
 package com.example.mumschedpoc.resources;
 
 import com.example.mumschedpoc.entities.User;
-import com.example.mumschedpoc.entities.enums.UserRole;
+import com.example.mumschedpoc.resources.dto.UpdateUserRequest;
+import com.example.mumschedpoc.resources.dto.UserCreationRequest;
+import com.example.mumschedpoc.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
+    private final UserService service;
+
+    public UserResource(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Ricardo", UserRole.ADMIN, "123", "password");
-        return ResponseEntity.ok(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody UserCreationRequest userRequest) {
+        User responseUser = service.insert(userRequest);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(responseUser.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(responseUser);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody UpdateUserRequest updateUserRequest) {
+        User user = service.update(id, updateUserRequest);
+        return ResponseEntity.ok(user);
     }
 
 }
