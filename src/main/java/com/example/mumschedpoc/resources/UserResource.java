4823c6184973a794636c08eaf77f1b6b398575f2
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-06 15:58:26 -0600

    Document user resource

--- src/main/java/com/example/mumschedpoc/resources/UserResource.java
+++ src/main/java/com/example/mumschedpoc/resources/UserResource.java
@@ -4,6 +4,11 @@ import com.example.mumschedpoc.entities.User;
 import com.example.mumschedpoc.resources.dto.UpdateUserRequest;
 import com.example.mumschedpoc.resources.dto.UserCreationRequest;
 import com.example.mumschedpoc.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name="User")
 public class UserResource {
 
     private final UserService service;
@@ -22,18 +28,27 @@ public class UserResource {
     }
 
     @GetMapping
+    @Operation(summary="List users")
     public ResponseEntity<List<User>> findAll() {
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
+    @Operation(summary="Get user", responses = {
+            @ApiResponse(description = "User found", responseCode = "200",
+            content = @Content(schema = @Schema(implementation = User.class))),
+
+            @ApiResponse(description = "User not found", responseCode = "404",
+                    content = @Content)
+    })
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
 
     @PostMapping
+    @Operation(summary="Add user")
     public ResponseEntity<User> insert(@RequestBody UserCreationRequest userRequest) {
         User responseUser = service.insert(userRequest);
 
@@ -44,12 +59,26 @@ public class UserResource {
     }
 
     @DeleteMapping(value = "/{id}")
+    @Operation(summary="Delete user", responses = {
+            @ApiResponse(description = "User deleted", responseCode = "200",
+                    content = @Content),
+
+            @ApiResponse(description = "User not found", responseCode = "404",
+                    content = @Content)
+    })
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping(value = "/{id}")
+    @Operation(summary="Update user", responses = {
+            @ApiResponse(description = "User updated", responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = User.class))),
+
+            @ApiResponse(description = "User not found", responseCode = "404",
+                    content = @Content)
+    })
     public ResponseEntity<User> update(@PathVariable Long id, @RequestBody UpdateUserRequest updateUserRequest) {
         User user = service.update(id, updateUserRequest);
         return ResponseEntity.ok(user);
