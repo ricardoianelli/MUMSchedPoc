4ef585f0d15205a9425e5c90531631a97ba7ff84
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-06 23:19:51 -0600

    Create Course service

---
+++ src/main/java/com/example/mumschedpoc/services/CourseService.java
@@ -0,0 +1,76 @@
+package com.example.mumschedpoc.services;
+
+import com.example.mumschedpoc.entities.Course;
+import com.example.mumschedpoc.repositories.CourseRepository;
+import com.example.mumschedpoc.resources.dto.CourseCreationRequest;
+import com.example.mumschedpoc.resources.dto.UpdateCourseRequest;
+import com.example.mumschedpoc.services.exceptions.DatabaseException;
+import com.example.mumschedpoc.services.exceptions.DuplicateResourceException;
+import com.example.mumschedpoc.services.exceptions.ResourceNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+    
+    private final CourseRepository repository;
+
+    public CourseService(CourseRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Course> findAll() {
+        return repository.findAll();
+    }
+
+    public Course findById(Integer id) {
+        Optional<Course> course = repository.findById(id);
+        return course.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Course insert(CourseCreationRequest courseRequest) {
+        try {
+            findById(courseRequest.id);
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            Course course = new Course(courseRequest.id, courseRequest.name);
+            return repository.save(course);
+        }
+        throw new DuplicateResourceException(courseRequest.id);
+    }
+
+    public void delete(Integer id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException ex) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException ex) {
+            throw new DatabaseException(ex.getMessage());
+        }
+    }
+
+    public Course update(Integer id, UpdateCourseRequest updateCourseRequest) {
+        try {
+            Course course = findById(id);
+            updateCourse(course, updateCourseRequest);
+            return repository.save(course);
+        } catch (EntityNotFoundException ex) {
+            throw new ResourceNotFoundException(id);
+        } catch (JpaObjectRetrievalFailureException ex) {
+            System.out.println(ex.getClass().getSimpleName());
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateCourse(Course course, UpdateCourseRequest updateCourseRequest) {
+        if (updateCourseRequest.name != null)
+            course.setName(updateCourseRequest.name);
+    }
+}
