19803fd8fced7b87d6afae4391eaec44c9a07615
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-06 18:04:49 -0600

    Create findByEmail method at UserRepository

--- src/main/java/com/example/mumschedpoc/repositories/UserRepository.java
+++ src/main/java/com/example/mumschedpoc/repositories/UserRepository.java
@@ -2,6 +2,13 @@ package com.example.mumschedpoc.repositories;
 
 import com.example.mumschedpoc.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT user FROM User user WHERE user.email = :user_email")
+    Optional<User> findByEmail(@Param("user_email")  String email);
 }
