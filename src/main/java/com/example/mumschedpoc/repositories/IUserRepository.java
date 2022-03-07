91a89a4cf20d5bc1ddeb5ca625f25e85c7d58334
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-07 14:47:11 -0600

    Rename repository interfaces

--- src/main/java/com/example/mumschedpoc/repositories/IUserRepository.java
+++ src/main/java/com/example/mumschedpoc/repositories/IUserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface IUserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT user FROM User user WHERE user.email = :user_email")
     Optional<User> findByEmail(@Param("user_email")  String email);
