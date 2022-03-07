32a6b04df368964221cb599c4aa450106a9c9632
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-06 21:19:10 -0600

    Improve login endpoint documentation

--- src/main/java/com/example/mumschedpoc/resources/LoginResource.java
+++ src/main/java/com/example/mumschedpoc/resources/LoginResource.java
@@ -2,8 +2,12 @@ package com.example.mumschedpoc.resources;
 
 import com.example.mumschedpoc.entities.User;
 import com.example.mumschedpoc.resources.dto.LoginRequest;
+import com.example.mumschedpoc.resources.exceptions.StandardError;
 import com.example.mumschedpoc.services.LoginService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +29,16 @@ public class LoginResource {
     }
 
     @PostMapping
-    @Operation(summary="Login")
+    @Operation(summary="User Login", responses = {
+            @ApiResponse(description = "Login Successful", responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = User.class))),
+
+            @ApiResponse(description = "User not found", responseCode = "404",
+                    content = @Content(schema = @Schema(implementation = StandardError.class))),
+
+            @ApiResponse(description = "Wrong password", responseCode = "401",
+                    content = @Content(schema = @Schema(implementation = StandardError.class)))
+    })
     public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
         User responseUser = service.login(loginRequest);
 
