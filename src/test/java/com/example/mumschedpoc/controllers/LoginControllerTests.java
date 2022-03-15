b8c0c2a4aa421b3813ca1f46f56236b0a7b300a4
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-15 17:14:37 -0500

    Refactor Login unit tests

--- src/test/java/com/example/mumschedpoc/controllers/LoginControllerTests.java
+++ src/test/java/com/example/mumschedpoc/controllers/LoginControllerTests.java
@@ -2,36 +2,69 @@ package com.example.mumschedpoc.controllers;
 
 import com.example.mumschedpoc.controllers.dto.LoginRequest;
 import com.example.mumschedpoc.entities.User;
-import com.example.mumschedpoc.services.LoginService;
+import com.example.mumschedpoc.entities.enums.UserRole;
+import com.example.mumschedpoc.services.exceptions.InvalidEmailException;
 import com.example.mumschedpoc.services.interfaces.ILoginService;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(LoginController.class)
 class LoginControllerTests {
 
-    @Mock
-    private ILoginService service = Mockito.mock(LoginService.class);
+    @MockBean
+    private ILoginService service;
 
-    @InjectMocks
-    private LoginController controller;
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Gson gson;
+
+    @BeforeEach
+    void setupTests()
+    {
+        gson = new Gson();
+    }
 
     @Test
-    void login_givenAValidUsernameAndPassword_ShouldReturnOk() {
+    void login_givenAValidUsernameAndPassword_ShouldReturnOk() throws Exception {
+        //Arrange
         LoginRequest request = new LoginRequest();
-        when(service.login(request)).thenReturn(new User());
+        User user = new User();
+        user.setUserRole(UserRole.ADMIN);
+        when(service.login(any(LoginRequest.class))).thenReturn(user);
 
-        ResponseEntity<User> response = controller.login(request);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        //Act
+        mockMvc.perform(post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(request))
+                        .accept(MediaType.APPLICATION_JSON))
+        //Assert
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void login_givenNotExistentUsername_ShouldReturn404() throws Exception {
+        //Arrange
+        when(service.login(any(LoginRequest.class))).thenThrow(new InvalidEmailException("email"));
+
+        //Act
+        mockMvc.perform(post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(new LoginRequest()))
+                        .accept(MediaType.APPLICATION_JSON))
+        //Assert
+                .andExpect(status().isNotFound());
     }
 
 }
