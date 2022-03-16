853b9270d68da229c7c079a9dd16b90d270c908d
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-16 17:25:05 -0500

    Update CourseDTO

--- src/main/java/com/example/mumschedpoc/dto/CourseDTO.java
+++ src/main/java/com/example/mumschedpoc/dto/CourseDTO.java
@@ -1,6 +1,24 @@
 package com.example.mumschedpoc.dto;
 
+import com.example.mumschedpoc.entities.Course;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CourseDTO {
+    public Integer id;
     public String code;
     public String name;
+    public String description;
+    public List<String> preRequisites;
+
+    public CourseDTO() {}
+
+    public CourseDTO(Course course) {
+        this.id = course.getId();
+        this.code = course.getCode();
+        this.name = course.getName();
+        this.description = course.getDescription();
+        this.preRequisites = course.getPreRequisites().stream().map(Course::getCode).collect(Collectors.toList());
+    }
 }
