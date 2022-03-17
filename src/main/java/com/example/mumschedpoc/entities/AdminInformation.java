d640f3cdf91e37d6924e80f6f911b1d33a506025
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-17 05:24:54 -0500

    Make AdminInformation serializable

--- src/main/java/com/example/mumschedpoc/entities/AdminInformation.java
+++ src/main/java/com/example/mumschedpoc/entities/AdminInformation.java
@@ -1,10 +1,11 @@
 package com.example.mumschedpoc.entities;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "tb_admin_information")
-public class AdminInformation {
+public class AdminInformation implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
