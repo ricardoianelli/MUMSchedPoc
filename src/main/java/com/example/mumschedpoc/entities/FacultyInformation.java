11e57ab253ac352ea82ae6c4f7ad3a360a56123b
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-17 04:09:55 -0500

    Create faculty relationship with n courses

--- src/main/java/com/example/mumschedpoc/entities/FacultyInformation.java
+++ src/main/java/com/example/mumschedpoc/entities/FacultyInformation.java
@@ -1,6 +1,8 @@
 package com.example.mumschedpoc.entities;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_faculty_information")
@@ -12,6 +14,13 @@ public class FacultyInformation {
     @OneToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name="faculty_preferred_courses",
+            joinColumns=@JoinColumn(name="faculty_information_id", referencedColumnName="id"),
+            inverseJoinColumns=@JoinColumn(name="course_id", referencedColumnName="id"))
+    private List<Course> preferredCourses = new ArrayList<>();
 
     public User getUser() {
         return user;
@@ -28,5 +37,21 @@ public class FacultyInformation {
 
     public Integer getId() {
         return id;
+    }
+
+    public List<Course> getPreferredCourses() {
+        return preferredCourses;
+    }
+
+    public void addPreferredCourse(Course course) {
+        this.preferredCourses.add(course);
+    }
+
+    public void removePreferredCourse(Course course) {
+        this.preferredCourses.remove(course);
+    }
+
+    public void setPreferredCourses(List<Course> preferredCourses) {
+        this.preferredCourses = preferredCourses;
     }
 }
