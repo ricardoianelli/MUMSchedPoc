2052c3adc28b05063bf97ef136c94376d8bbc6a3
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-16 21:54:16 -0500

    Create BlockCourse DTO

---
+++ src/main/java/com/example/mumschedpoc/dto/BlockCourseDTO.java
@@ -0,0 +1,20 @@
+package com.example.mumschedpoc.dto;
+
+import com.example.mumschedpoc.entities.BlockCourse;
+
+public class BlockCourseDTO {
+    public Integer id;
+    public CourseDTO course;
+    public Integer facultyId;
+    public Integer blockId;
+
+    public BlockCourseDTO() {}
+
+    public BlockCourseDTO(BlockCourse course) {
+        this.id = course.getId();
+        this.course = new CourseDTO(course.getCourse());
+        this.facultyId = course.getFaculty().getId();
+        this.blockId = course.getBlock().getId();
+    }
+
+}
