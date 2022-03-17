8c473f65108fd2ca92ce28cb8f44fa90ac3ea09f
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-16 23:20:06 -0500

    Add post endpoint for blocks

--- src/main/java/com/example/mumschedpoc/controllers/BlockController.java
+++ src/main/java/com/example/mumschedpoc/controllers/BlockController.java
@@ -5,10 +5,11 @@ import com.example.mumschedpoc.services.interfaces.IBlockService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,5 +27,17 @@ public class BlockController {
     public ResponseEntity<List<BlockDTO>> findAll() {
         List<BlockDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    @Operation(summary="Add block")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<BlockDTO> insert(@RequestBody BlockDTO blockDTO) {
+        BlockDTO response = service.insert(blockDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(response.id).toUri();
+
+        return ResponseEntity.created(uri).body(response);
     }
 }
