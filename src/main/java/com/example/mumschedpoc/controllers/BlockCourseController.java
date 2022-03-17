c3e2766f1d7987b72ee93aef5f1b82b41dd5b896
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-03-16 23:30:57 -0500

    Create add block course endpoint

--- src/main/java/com/example/mumschedpoc/controllers/BlockCourseController.java
+++ src/main/java/com/example/mumschedpoc/controllers/BlockCourseController.java
@@ -1,14 +1,16 @@
 package com.example.mumschedpoc.controllers;
 
 import com.example.mumschedpoc.dto.BlockCourseDTO;
+import com.example.mumschedpoc.dto.BlockDTO;
 import com.example.mumschedpoc.services.interfaces.IBlockCourseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,5 +28,17 @@ public class BlockCourseController {
     public ResponseEntity<List<BlockCourseDTO>> findAll() {
         List<BlockCourseDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    @Operation(summary="Add block course")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<BlockCourseDTO> insert(@RequestBody BlockCourseDTO blockCourseDTO) {
+        BlockCourseDTO response = service.insert(blockCourseDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(response.id).toUri();
+
+        return ResponseEntity.created(uri).body(response);
     }
 }
